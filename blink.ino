0bf3a8a1d39d27eb0468571d98f16570c99eee7f
Patrick Shriwise <pshriwise@gmail.com>
2018-09-01 19:14:18 -0500

    A simple blink program.

---
+++ blink.ino
@@ -0,0 +1,20 @@
+#define GREEN_LED 13
+#define BLUE_LED  12
+
+#define DELAY 500
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(GREEN_LED,OUTPUT);
+  pinMode(BLUE_LED,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(GREEN_LED,HIGH);
+  digitalWrite(BLUE_LED,HIGH);
+  delay(DELAY);
+  digitalWrite(GREEN_LED,LOW);
+  digitalWrite(BLUE_LED,LOW);
+  delay(DELAY);
+}
