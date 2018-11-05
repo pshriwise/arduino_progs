013715e8fa8bdf644cefc35fa6678b27f45fed6e
pshriwise <pshriwise@gmail.com>
2018-11-05 00:39:24 -0600

    It's working!

--- full_system/full_system.ino
+++ full_system/full_system.ino
@@ -5,6 +5,7 @@
 
 #define MIN_RANGE 4.0 // inches
 #define MAX_RANGE 8.0 // inches
+#define BUFFER    0.25 // inches
 
 // required delay to avoid overlapping signals
 #define TRIGGER_DELAY 70 // milliseconds
@@ -13,7 +14,7 @@
 #define NUM_READS 3
 
 // how long between distance reads
-#define TIME_BETWEEN_MEASUREMENT 2500 // milliseconds
+#define TIME_BETWEEN_MEASUREMENT 250 // milliseconds
 
 // MOTOR
 #define IN1 5
@@ -72,9 +73,9 @@ void setup() {
   pinMode(LED_CH, OUTPUT);
 
   //setup controls
-  pinMode(STP_BTN, INPUT);
-  pinMode(FWD_BTN, INPUT);
-  pinMode(BWD_BTN, INPUT);
+  pinMode(STP_BTN, INPUT_PULLUP);
+  pinMode(FWD_BTN, INPUT_PULLUP);
+  pinMode(BWD_BTN, INPUT_PULLUP);
   
   // setup output
   Serial.begin(9600);
@@ -114,36 +115,84 @@ void motor_stop() {
   analogWrite(ENA, 0);
 }
 
+bool motor_stop_check() {
+  // motor disable check
+  if (digitalRead(STP_BTN) == LOW) {
+    MOTOR_ENABLED = !MOTOR_ENABLED;
+    if (motor_disabled()) motor_stop();
+    press_wait(STP_BTN);
+  }
+  return motor_disabled();
+}
+
+void run_forward() {
+  motor_forward();
+
+  double dist;
+  while(true) {
+    if (motor_stop_check()) return;
+
+    dist = dist_read();
+
+    if ( dist > MAX_RANGE || dist < MIN_RANGE - BUFFER ) {
+      motor_stop();
+      return;
+    }
+    
+    if (digitalRead(FWD_BTN) == LOW) {
+      press_wait(FWD_BTN);
+      motor_stop();
+      return;
+    }
+    
+  }
+}
+
+void run_backward() {
+  motor_backward();
+
+  double dist;
+  while(true) {
+    if (motor_stop_check()) return;
+
+    dist = dist_read();
+
+    if ( dist < MIN_RANGE || dist > MAX_RANGE + BUFFER ) {
+      motor_stop();
+      return;
+    }
+
+    if (digitalRead(BWD_BTN) == LOW) {
+      press_wait(BWD_BTN);
+      motor_stop();
+      return;
+    }
+  }
+}
+
+
 // "main"
 void loop() {
 
-  // motor disable check
-  if (digitalRead(STP_BTN)) {
-    MOTOR_ENABLED = !MOTOR_ENABLED;
-    if (motor_disabled()) motor_stop();
-    press_wait();
-  }
-    
-  // read echo and print to console
-  double dist = dist_read();
 
-  //  digitalWrite(LED_CH, HIGH);
+  motor_stop_check();
   
-  if (dist <= MAX_RANGE && dist >= MIN_RANGE) {
-    digitalWrite(LED_CH, HIGH);
-    motor_forward();
-  } else {
-    digitalWrite(LED_CH, LOW);
-    motor_stop();
+  if (digitalRead(FWD_BTN) == LOW) {
+    press_wait(FWD_BTN);
+    run_forward();
   }
-  
+
+  if (digitalRead(BWD_BTN) == LOW) {
+    press_wait(BWD_BTN);
+    run_backward();
+  }
   // wait, and read again
-  //  delay(TIME_BETWEEN_MEASUREMENT);
+  delay(TIME_BETWEEN_MEASUREMENT);
 }
 
   // wait for button state to return to low
-void press_wait() {
+void press_wait(int channel) {
   while (true) {
-    if (!digitalRead(STP_BTN)) { return; }
+    if (digitalRead(channel) == HIGH) { return; }
   }
 }
