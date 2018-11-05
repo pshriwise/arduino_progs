2ed290d1970407bc5550f946beedbca86d259e33
pshriwise <pshriwise@gmail.com>
2018-11-05 00:43:22 -0600

    Adding a button to the toggle program.

--- btn_toggle/btn_toggle.ino
+++ btn_toggle/btn_toggle.ino
@@ -1,37 +1,65 @@
-#define LED_PIN 13
-#define BUTTON_PIN 2
+#define LEDA_PIN 12
+#define LEDB_PIN 13
+
+#define BUTTONA_PIN 2
+#define BUTTONB_PIN 3
 
 // variables
 bool led_state = false;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT);
+  pinMode(LEDA_PIN, OUTPUT);
+  pinMode(BUTTONA_PIN, INPUT_PULLUP);
+  pinMode(LEDB_PIN, OUTPUT);
+  pinMode(BUTTONB_PIN, INPUT_PULLUP);
+
 }
 
 void loop() {
 
-  if (digitalRead(BUTTON_PIN)) {
-    press_wait();
+  if (digitalRead(BUTTONA_PIN) == LOW) {
+    press_wait_a();
     led_state = !led_state;
 
     // update led status
     if (led_state) {
       // turn LED on
-      digitalWrite(LED_PIN, HIGH);
+      digitalWrite(LEDA_PIN, HIGH);
     } else {
       // turn LED off
-      digitalWrite(LED_PIN, LOW);
+      digitalWrite(LEDA_PIN, LOW);
     }
 
   }
 
+  if (digitalRead(BUTTONB_PIN) == LOW) {
+    press_wait_b();
+    led_state = !led_state;
+
+    // update led status
+    if (led_state) {
+      // turn LED on
+      digitalWrite(LEDB_PIN, HIGH);
+    } else {
+      // turn LED off
+      digitalWrite(LEDB_PIN, LOW);
+    }
+
+  }
+
+  
 }
 
 // wait for button state to return to low
-void press_wait() {
+void press_wait_a() {
   while (true) {
-    if (!digitalRead(BUTTON_PIN)) { return; }
+    if (digitalRead(BUTTONA_PIN) == HIGH) { return; }
+  }
+}
+
+void press_wait_b() {
+  while (true) {
+    if (digitalRead(BUTTONB_PIN) == HIGH) { return; }
   }
 }
